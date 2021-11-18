66ea164b077d32b9c3b6f3da419c35323b1ff352
Tung Nguyen <nvntung@gmail.com>
2021-11-18 08:18:08 +0000

    Remove redundant tags

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<?php
-    $message = "
+$message = "
 <h2>Hello, I am a newbie with PHP programming. Please help me!!!</h2>";
-    echo $message;
+echo $message;
 
-    $data = array('shopid' => 3,'version' => 1, 'value' => 1);  //here $data is dummy variable
+$data = array('shopid' => 3,'version' => 1, 'value' => 1);  //here $data is dummy variable
 
-    $log_file = $_SERVER['DOCUMENT_ROOT']."/errors-".date("Y-n-j").".log";
+$log_file = $_SERVER['DOCUMENT_ROOT']."/errors-".date("Y-n-j").".log";
 
-    error_log(print_r($data,true), 3, $log_file);
+error_log(print_r($data,true), 3, $log_file);
 
-    // In $data we can mention the error message and create the log
-
-?>
+// In $data we can mention the error message and create the log
