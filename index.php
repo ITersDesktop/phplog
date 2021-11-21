cf709dd819cafc66d55494e4156757d94fab87ce
Tung Nguyen <nvntung@gmail.com>
2021-11-21 22:25:54 +0000

    Add the missing required open php tag

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<?php
 $message = "
 <h2>Hello, I am a newbie with PHP programming. Please help me!!!</h2>";
 echo $message;
 
-$data = array('shopid' => 3,'version' => 1, 'value' => 1);  //here $data is dummy variable
+$data = array('shopid' => 3,'version' => 1, 'value' => 2);  //here $data is dummy variable
 
 $log_file = $_SERVER['DOCUMENT_ROOT']."/errors-".date("Y-n-j").".log";
 
